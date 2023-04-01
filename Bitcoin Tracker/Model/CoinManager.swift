0bfcda727628acb7fca9af5c81790c085b824834
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-01 15:43:36 -0700

    feat: Create manager file

---
+++ Bitcoin Tracker/Model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  Bitcoin Tracker
+//
+//  Created by Grayson Ruffo on 2023-04-01.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
