6e7736fb8af16a7080fd369d112d26b23b6aaba4
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-01 19:09:02 -0700

    feat: Create method to fetch api key from plist

--- Bitcoin Tracker/Model/CoinManager.swift
+++ Bitcoin Tracker/Model/CoinManager.swift
@@ -10,9 +10,38 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    var apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    init() {
+        apiKey = getPlistValueWithKey(fileName: "APIKey", keyName: "coinAPIKey")
+    }
+    
+    mutating func getPlistValueWithKey(fileName: String, keyName: String) -> String {
+        var plist: [String: String]?
+        if let apiKeyPlistPath = Bundle.main.url(forResource: fileName, withExtension: "plist") {
+            do {
+                let apiKeyPlistData = try Data(contentsOf: apiKeyPlistPath)
+                
+                if let dict = try PropertyListSerialization.propertyList(from: apiKeyPlistData, options: [], format: nil) as? [String: String] {
+                    plist = dict
+                    
+                    if let value = plist?[keyName] {
+                        return value
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }
+        return "Key Name Not Found"
+    }
+    
+    
+    func getCoinPrice(for currency: String) {
+        
+    }
 
     
 }
